3352d3f2194e90ea2c5d456e7fce530de9578614
Thanh LeMinh(thanhlm1) <thanhlm1@smartosc.com>
2023-11-21 17:36:22 +0700

    change 21/11

---
+++ Sitecore.Demo.Project.MVC.Web/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Demo.Project.MVC.Web.Models;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Demo.Project.MVC.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            var datasourceItem = RenderingContext.Current?.Rendering.Item;
+            var model = new AboutViewModel()
+            {
+                Item = RenderingContext.Current?.Rendering.Item,
+            };
+            return View();
+        }
+    }
+}
